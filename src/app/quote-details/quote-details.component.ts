eca6df0cb5abff3ed49f3d3392b0bc7df75be3a9
beth nduta <bethnduta05@gmail.com>
2022-04-16 14:10:37 +0300

    changed quote component

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
