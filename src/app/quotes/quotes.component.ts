52c82811328243f4890ca06b42e6e41821de3171
beth nduta <bethnduta05@gmail.com>
2022-04-19 11:31:01 +0300

    modified app quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,14 +10,17 @@ export class QuotesComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, 'if you dont like someones story write yours', 'chinua achebe', 'beth nduta',0,0),
     new Quote(2, 'Nobody can teach me who I am', 'chinua achebe', 'beth nduta',0,0),
-    new Quote(3, 'The world id like a mask dancing', 'chinua achebe', 'beth nduta',0,0),
-    // new Quote(4, 'Time is money', 'Ian Sang', 'beth nduta', 0, 0),
-    // new Quote(5, 'Time is money', 'Ian Sang', 'beth nduta',0,0),
+    new Quote(3, 'The world id like a mask dancing', 'chinua', 'beth nduta',0,0),
+    new Quote(4, 'Time is money', 'Ian Sang', 'beth nduta', 0, 0),
+    new Quote(5, 'Time is money', 'Ian Sang', 'beth nduta',0,0),
   ];
   addNewQuote(quote:Quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     this.quotes.push(quote)
+  }
+    quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
   }
   constructor() { }
 
