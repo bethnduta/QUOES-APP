8e6e058464980265c4e33b8074389125ae1a3d3f
beth nduta <bethnduta05@gmail.com>
2022-04-22 17:00:39 +0300

    Auto-generated commit [ci skip]

--- src/app/quote.ts
+++
@@ -1,5 +0,0 @@
-export class Quote {
-    constructor(public id:number, public quote:string, public author:string, public name:string, public upvote:number, public downvote:number) {
-        
-    }
-}
